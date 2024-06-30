ecb8fce0434aee9e37f4daa380660af086cce7e5
kathy2470 <mukisakathy@gmail.com>
2024-06-30 20:42:24 +0300

    2-arrow.js

--- 0x00-ES6_basic/1-block-scoped.test.js
+++ 0x00-ES6_basic/1-block-scoped.test.js
@@ -1,11 +1,13 @@
 import taskBlock from './1-block-scoped.js';
 
-describe('taskBlock', () => {
-  test('returns [false, true] when trueOrFalse is true', () => {
-    expect(taskBlock(true)).toEqual([false, true]);
+describe('taskBlock function', () => {
+  it('should return [true, false] when trueOrFalse is true', () => {
+    const result = taskBlock(true);
+    expect(result).toEqual([true, false]);
   });
 
-  test('returns [false, true] when trueOrFalse is false', () => {
-    expect(taskBlock(false)).toEqual([false, true]);
+  it('should return [false, true] when trueOrFalse is false', () => {
+    const result = taskBlock(false);
+    expect(result).toEqual([false, true]);
   });
 });
