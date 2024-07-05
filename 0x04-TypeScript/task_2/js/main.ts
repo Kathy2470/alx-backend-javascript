3c25c639272e7341ecfddf39649405986575bdc4
kathy2470 <mukisakathy@gmail.com>
2024-07-05 14:08:36 +0300

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -4,7 +4,6 @@ export interface DirectorInterface {
   workDirectorTasks(): string;
 }
 
-// TeacherInterface with the required methods
 export interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
@@ -12,36 +11,58 @@ export interface TeacherInterface {
 }
 
 export class Director implements DirectorInterface {
-  workFromHome(): string {
-    return "Working from home";
+  workFromHome() {
+    return 'Working from home';
   }
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";
+
+  getCoffeeBreak() {
+    return 'Getting a coffee break';
   }
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
+
+  workDirectorTasks() {
+    return 'Getting to director tasks';
   }
 }
 
 export class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return "Cannot work from home";
+  workFromHome() {
+    return 'Cannot work from home';
   }
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
+
+  getCoffeeBreak() {
+    return 'Cannot have a break';
   }
-  workTeacherTasks(): string {
-    return "Getting to work";
+
+  workTeacherTasks() {
+    return 'Getting to work';
   }
 }
 
-export function createEmployee(salary: number | string): Director | Teacher {
+export function createEmployee(salary: (number | string)): (Director | Teacher) {
   if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
 }
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+export function isDirector(employee: (Director | Teacher)) {
+  return employee instanceof Director;
+}
+
+export function executeWork(employee: (Director | Teacher)) {
+  if (isDirector(employee)) {
+    return (employee as Director).workDirectorTasks();
+  }
+  return (employee as Teacher).workTeacherTasks();
+}
+
+export type Subjects = ('Math' | 'History');
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
