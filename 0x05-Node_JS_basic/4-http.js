6558539d22f31b0e2acdb08ff37838b9540b4174
kathy2470 <mukisakathy@gmail.com>
2024-08-24 01:07:59 +0300

    Task 4 update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,20 @@
 const http = require('http');
 
-// Create the HTTP server
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-// Start the server on port 1245
-app.listen(1245, () => {
-  console.log('Server running at http://localhost:1245/');
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
-// Export the app variable
 module.exports = app;
